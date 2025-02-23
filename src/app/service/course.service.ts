481ee7f669d403e5ced958b9b1d79e0ae2fe560f
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-02-23 09:22:34 +0530

    SERVICES FUNCTIONS ADDED

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,19 @@ export class CourseService {
 
   constructor() { }
 
-  
+  // getAll():String[] {
+  //   return [('defre','hello','hello')]
+  // }
+
+  getAll(): Course[] {
+    return [
+     { id:1,
+      name: 'NAVODYA',
+      description: 'Year 4', },
+
+     { id:2,
+      name: 'DIVYANJALI',
+      description: 'Year 4', },
+    ];
+  }
 }
