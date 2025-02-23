1cc77082e132b5f1c5c641e41384fc24996a9b0d
divyanjali2 <navodyadivyanjali2@gmail.com>
2025-02-23 09:27:06 +0530

    course details updated

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -15,12 +15,24 @@ export class CourseService {
   getAll(): Course[] {
     return [
      { id:1,
-      name: 'NAVODYA',
-      description: 'Year 4', },
+      name: 'Intro to Computer Science',
+      description: 'This course focuses on advanced concepts in its field and includes practical projects.' },
 
      { id:2,
-      name: 'DIVYANJALI',
-      description: 'Year 4', },
+      name: 'Advanced Programming',
+      description: 'Designed for final-year students, this course covers comprehensive subject material.' },
+
+     { id:3,
+      name: 'Data Structures and Algorithms',
+      description: 'An intermediate course aimed at building foundational skills and knowledge.' },
+
+     { id:4,
+      name: 'Web Development Basics',
+      description: 'An introductory course perfect for beginners to start learning the basics.' },
+
+     { id:5,
+      name: 'Software Engineering Principles',
+      description: 'This course bridges intermediate and advanced concepts with hands-on assignments.' }
     ];
   }
 }
